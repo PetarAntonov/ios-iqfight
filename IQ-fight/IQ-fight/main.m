802d020398c44eb576660af0e1fdca6dfaab2b12
pesho11 <peshkata_cool@abv.bg>
2014-04-21 16:31:50 +0300

    init

---
+++ IQ-fight/IQ-fight/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IQ-fight
+//
+//  Created by Petar Antonov on 4/21/14.
+//  Copyright (c) 2014 Petar Antonov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IQAppDelegate class]));
+    }
+}
